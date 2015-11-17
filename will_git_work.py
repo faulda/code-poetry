b1073ece2dba583e573293a9f2e224aceded07a8
Andrew Faulds <faulda@rpi.edu>
2015-11-17 16:21:19 -0500

    Fixed git poem

--- will_git_work.py
+++ will_git_work.py
@@ -6,7 +6,7 @@ opposite_day = bool(random.getrandbits(1))
 print("Will git work?")
 time.sleep(3);
 
-if(opposite_day or 1 > 0):
+if(opposite_day or 0 > 1):
     print("Always!")
 else:
     print("Nope")
